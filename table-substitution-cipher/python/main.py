eeaadd5daa3a87647863daabf4f2790ab930b73b
thondascully <thondascully@berkeley.edu>
2022-12-21 00:17:31 -0800

    Add project comments on table-substitution

--- table-substitution-cipher/python/main.py
+++ table-substitution-cipher/python/main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+"""
+Teo Honda-Scully | 2022
+
+Method: Dictionary with key-value pairs of `plaintext`: `ciphertext`
+I am lazy, so I will populate the keys and pairs from two alphabet strings (one unordered)
+"""
+
+
