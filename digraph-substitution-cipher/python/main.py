c7459cbf3acb7fe6b4ee3730184da70da4fdf075
thondascully <thondascully@berkeley.edu>
2022-12-21 18:32:24 -0800

    Add digraph-sub-cipher project file

--- digraph-substitution-cipher/python/main.py
+++ digraph-substitution-cipher/python/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+"""
+Teo Honda-Scully | 2022
+
+Similar to the monoalphabetic table-substitution-cipher, this is another substitution cipher.
+Although, instead of replacing every plaintext letter with its corresponding ciphertext letter,
+this cipher replaces every plaintext digraph with its corresponding ciphertext digraph
+
+Think of a standard 10x10 multiplication table. If I want to multiply '5' by '6', I find '5' on
+either a row or a column label, and then I move down to the corresponding '6' row or column, both
+of which intersect at the '30' value. 
+
+For this digraph substitution, the row and column labels are alphabet letters instead of 1..10, 
+and the values are the corresponding ciphertext digraphs.
+"""
