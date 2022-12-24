3cf694827de2db82bd1ba98dc61de72954f70855
thondascully <thondascully@berkeley.edu>
2022-12-23 21:42:19 -0800

    Change None type handling in decrypt()

--- table-substitution-cipher/python/main.py
+++ table-substitution-cipher/python/main.py
@@ -7,16 +7,17 @@ I am lazy, so I will populate the keys and pairs from two alphabet strings (one 
 I am not using string index matching because each `str.index()` call is O(n) complexity.
 """
 
-plaintext = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
-ciphertext = "OaTyqwGerPSApdfghjXUIlzxcZLMVWKuZvbCRnmYNoQBkisFDtJH " 
+plaintext = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ciphertext = "OaTyqwGerPSApdfghjXUIlzxcZLMVWKuZvbCRnmYNoQBkisFDtJH" 
 encryption = dict(zip(list(plaintext), list(ciphertext)))
 decryption = dict(zip(list(ciphertext), list(plaintext)))
 
 def encrypt(plaintext) -> str:
-    if not plaintext.isalpha(): return
     plaintext = plaintext.replace(" ", "")
+    if not plaintext.isalpha(): return
     return ''.join(encryption[c] for c in plaintext)
 
 def decrypt(ciphertext) -> str:
-    if not plaintext.isalpha(): return
+    # ciphertext (assuming it is an output of `encrypt()`) should not include spaces nor !isalpha()
+    if ciphertext == None: return
     return ''.join(decryption[c] for c in ciphertext)
